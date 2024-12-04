2adc98e8924eb20d9055490827d75c0bc86e5ac2
cmd-teemo <leonguyen8@yahoo.com>
2024-12-03 17:12:27 -0800

    Refresh Main

--- main.cpp
+++ main.cpp
@@ -1,37 +1,16 @@
 /* This is the main execution file */
 #include <iostream>
-#include <fstream>
 #include <set>
 #include <map>
 #include <string>
-#include <cctype>
 #include <algorithm>
-#include <ctime>
 #include <cstdlib>
 #include <vector>
-#include <filesystem>
-#include <SFML/Graphics.hpp>
+
+
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Photo Viewer");
-    sf::Texture texture;
-
-    if (!texture.loadFromFile("example.jpg")) {
-        return -1; // Exit if the file could not be loaded
-    }
-
-    sf::Sprite sprite(texture);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(sprite);
-        window.display();
+    
     }
 
     return 0;
