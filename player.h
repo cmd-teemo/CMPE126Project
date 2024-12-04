1bf5370b76e674a2ceba3603854fcc54363c4aac
cmd-teemo <leonguyen8@yahoo.com>
2024-12-03 17:25:15 -0800

    Player Age Updated/ Remove SFML

--- player.h
+++ player.h
@@ -6,14 +6,15 @@ private:
     std::string name;
     int number;
     std::string role; // Changed from status to role
+    int age;
 
 public:
     // Default constructor
-    Player() : name("Unknown"), number(0), role("Benched") {} // Default role set to "Bench"
+    Player() : name("Unknown"), number(0), role("Benched"), age(0) {} // Default role set to "Bench"
 
     // Parameterized constructor
-    Player(std::string playerName, int playerNumber, std::string playerRole) 
-        : name(playerName), number(playerNumber), role(playerRole) {}
+    Player(std::string playerName, int playerNumber, std::string playerRole, int playerAge) 
+        : name(playerName), number(playerNumber), role(playerRole), age(playerAge) {}
 
     // Function to set player name
     void setName(const std::string& playerName) {
@@ -44,12 +45,19 @@ public:
     std::string getRole() const {
         return role;
     }
+    int getAge() const {
+        return age;
+    }
 
+    void setAge(int playerAge) {
+        age = playerAge;
+    }
     // Method to display player information
     void displayInfo() const {
         std::cout << "Player Name: " << name << std::endl;
         std::cout << "Player Number: " << number << std::endl;
         std::cout << "Player Role: " << role << std::endl; // Updated to role
+        std::cout << "Player Age: " << age << std::endl;
     }
 };
 
